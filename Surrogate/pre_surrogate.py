26633625c04c1effc7a58baa4c005e02d43ce6d6
Jianfeng <ginfungchan@gmail.com>
2016-02-04 15:59:41 -0500

    f

--- Surrogate/pre_surrogate.py
+++ Surrogate/pre_surrogate.py
@@ -1,5 +1,22 @@
+import pdb
+from os import sys, path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from Parser.parser import FTModel
+
 __author__ = "Jianfeng Chen"
 __copyright__ = "Copyright (C) 2016 Jianfeng Chen"
 __license__ = "MIT"
 __version__ = "1.0"
 __email__ = "jchen37@ncsu.edu"
+
+
+def main(name):
+    m = FTModel(name, setConVioAsObj=False)
+    m.printModelInfo()
+    # can = m.genRandomCan(guranteeOK=True)
+    # m.eval(can,doNorm=False)
+    pdb.set_trace()
+
+
+if __name__ == '__main__':
+    main('eshop')
--- parser/__init__.py
+++ parser/__init__.py
@@ -1,1 +1,5 @@
-__author__ = 'Jianfeng'
+__author__ = "Jianfeng Chen"
+__copyright__ = "Copyright (C) 2016 Jianfeng Chen"
+__license__ = "MIT"
+__version__ = "1.0"
+__email__ = "jchen37@ncsu.edu"
