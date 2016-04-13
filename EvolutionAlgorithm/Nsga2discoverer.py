5799a6c059d7501613b4bf7e22baace7244b77a9
Jianfeng <ginfungchan@gmail.com>
2016-04-12 23:00:06 -0400

    rs

--- EvolutionAlgorithm/Nsga2discoverer.py
+++ EvolutionAlgorithm/Nsga2discoverer.py
@@ -5,6 +5,7 @@ from FeatureModel.discoverer import Discoverer
 from Observers import individuals_observer
 import random
 import ecspy
+import time
 import pdb
 
 __author__ = "Jianfeng Chen"
@@ -14,7 +15,7 @@ __version__ = "2.0"
 __email__ = "jchen37@ncsu.edu"
 
 
-class NsgaIIDiscover(Discoverer):
+class Nsga2Discover(Discoverer):
     def __init__(self, feature_model):
         # check whether 'conVio' set as an objective
         if 'conVio' not in [o.name for o in feature_model.obj]:
@@ -31,34 +32,55 @@ class NsgaIIDiscover(Discoverer):
 
     def gen_valid_one(self):
         problem = self.ecs_model
+        final_pop = self.ea.evolve(generator=problem.generator,
+                                   evaluator=problem.evaluator,
+                                   pop_size=500,
+                                   maximize=problem.maximize,
+                                   bounder=problem.bounder,
+                                   num_selected=100,
+                                   max_generations=5,
+                                   mutation_rate=0.3)
+        valid_pop = filter(lambda p: p.fitness[1] == 0, final_pop)
+        return EcsFTModel.ecs_individual2ft_candidate(valid_pop[0])
+
+    def run(self):
+        problem = self.ecs_model
         self.ea.observer = [individuals_observer]
 
-        ind_file_name = 'inds.csv'
+        ind_file_name = '{0}/Records/NSGA2_{1}_{2}_inds.csv'.format(project_path, self.feature_model.name,
+                                                                    time.strftime('%y%m%d'))
+
         ind_file = open(ind_file_name, 'w')
-        stat_file_name = 'stat.csv'
+        stat_file_name = '{0}/Records/NSGA2_{1}_{2}_stat.csv'.format(project_path, self.feature_model.name,
+                                                                     time.strftime('%y%m%d'))
         stat_file = open(stat_file_name, 'w')
 
         final_pop = self.ea.evolve(generator=problem.generator,
                                    evaluator=problem.evaluator,
-                                   pop_size=100,
+                                   pop_size=1000,
                                    maximize=problem.maximize,
                                    bounder=problem.bounder,
-                                   num_selected=100,
-                                   max_generations=10,
+                                   max_generations=50,
                                    mutation_rate=0.3,
                                    individuals_file=ind_file,
                                    statistics_file=stat_file)
         ind_file.close()
-        valid_pop = filter(lambda p: p.fitness[1] == 0, final_pop)
-        return valid_pop
+        stat_file.close()
 
 
 def demo(name):
-    g = NsgaIIDiscover(FTModel(name, setConVioAsObj=True))
-    ff = g.gen_valid_one()
-    qq = map(EcsFTModel.ecs_individual2ft_candidate, ff)
-    pdb.set_trace()
+    g = Nsga2Discover(FTModel(name, setConVioAsObj=True))
+    g.run()
 
 if __name__ == '__main__':
-    demo('webportal')
+    import traceback
+    try:
+        # for n in ['webportal','eshop', 'eis']:
+        for n in ['eshop', 'eis']:
+            demo(n)
+            print n
+    except:
+        type, value, tb = sys.exc_info()
+        traceback.print_exc()
+        pdb.post_mortem(tb)
 
---
+++ plotRecords.py
@@ -0,0 +1,159 @@
+from __future__ import division
+import pdb
+import csv
+import matplotlib.pyplot as plt
+from os import sys, path, listdir
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+__author__ = "Jianfeng Chen"
+__copyright__ = "Copyright (C) 2016 Jianfeng Chen"
+__license__ = "MIT"
+__version__ = "1.0"
+__email__ = "jchen37@ncsu.edu"
+
+
+project_path = filter(lambda x: x.endswith('SPL'), sys.path)[0]
+
+
+def contains(s, lst):
+    """
+    determine whether s contains any element in the lst. Return TRUE as long as it contains at least one element.
+    :param s:
+    :param lst:
+    :return:
+    """
+    for l in lst:
+        if l in s:
+            return True
+    return False
+
+
+def draw_valid_can_generate_rate(model):
+    plt.clf()
+    ea_records = []
+    fmm_records = []
+    surrogate_records = []
+
+    lines = []
+
+    # classify the files
+    for f in listdir(project_path+'/Records/'):
+        if 'stat' not in f:
+            continue
+        if model in f and contains(f, ['GA', 'NSGA3']):
+            ea_records.append(f)
+        if model in f and contains(f, ['m2']):
+            fmm_records.append(f)
+
+    for ear in ea_records:
+        with open(project_path+'/Records/'+ear, 'r') as f:
+            reader = csv.reader(f)
+            rows = [row for row in reader][1:]  # ignore the csv header
+            cols = map(list, zip(*rows))
+        evaluations = map(int, cols[1])
+        valid_rate = map(float, cols[2])
+        # hv = map(float, cols[3])
+        timestamp = map(float, cols[4])
+        start_time = min(timestamp)
+        timestamp = map(lambda x: x-start_time, timestamp)
+
+        num_valid_cans = map(lambda r: int(r*(evaluations[1]-evaluations[0])), valid_rate)
+        time = []
+        rate = []
+        for n,s,e in zip(num_valid_cans[1:], timestamp[:-1], timestamp[1:]):
+            time.append((s+e)/2)
+            rate.append(n/(e-s)*60)
+        lines.append((time, rate, ear.split('_')[0]))
+
+    for fmm in fmm_records:
+        with open(project_path + '/Records/' + fmm, 'r') as f:
+            reader = csv.reader(f)
+            rows = [row for row in reader][1:]  # ignore the csv header
+            cols = map(list, zip(*rows))
+
+        num_valid_cans = cols[1]
+        num_valid_cans = map(int, num_valid_cans)
+
+        timestamp = map(float, cols[4])
+        start_time = min(timestamp)
+        timestamp = map(lambda x: x - start_time, timestamp)
+
+        time = []
+        rate = []
+        for n, s, e in zip(num_valid_cans[1:], timestamp[:-1], timestamp[1:]):
+            time.append((s + e) / 2)
+            rate.append(n / (e - s) * 60)
+        lines.append((time, rate, fmm.split('_')[0]))
+
+    # plt.axis((0, 300, 0, 1000))
+    for l in lines:
+        plt.plot(l[0], l[1], label=l[2])
+    plt.title(model + ":Candidate gen speed (#valid candidates/min)")
+    plt.legend()
+    plt.savefig(project_path+'/report/'+model+'_can_gen_speed')
+    # plt.show()
+
+
+def draw_hv_evolve(model):
+    plt.clf()
+    ea_records = []
+    fmm_records = []
+    surrogate_records = []
+
+    lines = []
+
+    # classify the files
+    for f in listdir(project_path + '/Records/'):
+        if 'stat' not in f:
+            continue
+        if model in f and contains(f, ['GA', 'NSGA3']):
+            ea_records.append(f)
+        if model in f and contains(f, ['m2']):
+            fmm_records.append(f)
+
+    for ear in ea_records:
+        with open(project_path + '/Records/' + ear, 'r') as f:
+            reader = csv.reader(f)
+            rows = [row for row in reader][1:]  # ignore the csv header
+            cols = map(list, zip(*rows))
+        hv = map(float, cols[3])
+        timestamp = map(float, cols[4])
+        start_time = min(timestamp)
+        timestamp = map(lambda x: x - start_time, timestamp)
+
+        time = []
+        hv_evlove = []
+        for h, s, e in zip(hv[1:], timestamp[:-1], timestamp[1:]):
+            time.append((s + e) / 2)
+            hv_evlove.append(h)
+        lines.append((time, hv_evlove, ear.split('_')[0]))
+
+    for fmm in fmm_records:
+        with open(project_path + '/Records/' + fmm, 'r') as f:
+            reader = csv.reader(f)
+            rows = [row for row in reader][1:]  # ignore the csv header
+            cols = map(list, zip(*rows))
+
+        hv = map(float, cols[3])
+        timestamp = map(float, cols[4])
+        start_time = min(timestamp)
+        timestamp = map(lambda x: x - start_time, timestamp)
+
+        time = []
+        hv_evlove = []
+        for h, s, e in zip(hv[1:], timestamp[:-1], timestamp[1:]):
+            time.append((s + e) / 2)
+            hv_evlove.append(h)
+        lines.append((time, hv_evlove, fmm.split('_')[0]))
+
+    # plt.axis((0, 300, 0, 1000))
+    for l in lines:
+        plt.plot(l[0], l[1], label=l[2])
+    plt.title(model + ":Hypervolume evolve")
+    plt.legend()
+    plt.savefig(project_path + '/report/' + model + '_can_hv_evolve')
+    # plt.show()
+
+for model in ['eis']:
+    draw_valid_can_generate_rate(model)
+    draw_hv_evolve(model)
